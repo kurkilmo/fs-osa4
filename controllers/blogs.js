e02d4541ed0314b982035408130ce9ada08ba440
kurkilmo <ilmo.kurki00@gmail.com>
2024-06-07 20:29:09 +0300

    Tehtävä 4.8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,17 +1,16 @@
 const Blog = require('../models/blog')
 const blogRouter = require('express').Router()
 
-blogRouter.get('', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
+  // const result = await blog.save()
+  // response.status(201).json(result)
   blog
     .save()
     .then(result => {
---
+++ tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const { test, after, describe, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('note-api', () => {
+  test('blogs are in JSON form', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('returns correct amount of blogs', async () => {
+    const resp = await api.get('/api/blogs')
+    assert.strictEqual(resp.body.length, helper.initialBlogs.length)
+  })
+
+  after(async () => {
+    await mongoose.connection.close()
+  })
+})
