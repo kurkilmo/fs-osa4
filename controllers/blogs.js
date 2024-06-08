f43e496479db59000fbd23d0e73094aab268bc6b
kurkilmo <ilmo.kurki00@gmail.com>
2024-06-08 22:11:39 +0300

    Tehtävät 4.21 & 4.22

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,21 +1,16 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const { modifyVerification, userExtractor } = require('../utils/middleware')
+//const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
-blogRouter.post('/', async (request, response) => {
+blogRouter.post('/', userExtractor, async (request, response) => {
   const body = request.body
-
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'invalid token' })
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog({
     likes: body.likes || 0,
@@ -30,12 +25,12 @@ blogRouter.post('/', async (request, response) => {
   response.status(201).json(result)
 })
 
-blogRouter.delete('/:id', async (request, response) => {
+blogRouter.delete('/:id', userExtractor, modifyVerification, async (request, response) => {
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
 
-blogRouter.put('/:id', async (request, response) => {
+blogRouter.put('/:id', userExtractor, modifyVerification, async (request, response) => {
   const body = request.body
 
   const updatedBlog = await Blog.findByIdAndUpdate(
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,7 @@
 const logger = require('./logger')
+const User = require('../models/user')
+const Blog = require('../models/blog')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -10,6 +13,40 @@ const requestLogger = (request, response, next) => {
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    req.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
+const userExtractor = async (req, res, next) => {
+  const decodedToken = jwt.verify(req.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'invalid token' })
+  }
+  const user = await User.findById(decodedToken.id)
+  req.user = user
+  next()
+}
+
+const modifyVerification = async (req, res, next) => {
+  const id = req.params.id
+  const blogToAlter = await Blog.findById(id)
+  if (!blogToAlter) return res.status(404).json({
+    error: 'id not found'
+  })
+
+  if (blogToAlter.user.toString() !== req.user.id) {
+    return res.status(401).json({
+      error: 'you don\'t have permission to modify this blog'
+    })
+  }
+
+  next()
 }
 
 const errorHandler = (error, request, response, next) => {
@@ -28,17 +65,11 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    req.token = authorization.replace('Bearer ', '')
-  }
-  next()
-}
-
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-  tokenExtractor
+  tokenExtractor,
+  userExtractor,
+  modifyVerification
 }
